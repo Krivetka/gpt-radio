3078711134500c71aff3bb90462c04a07f8ed3bd
i.kuliashov <ivan.kuliashov@student.ehu.lt>
2024-10-14 19:17:55 +0200

    feat: update button

--- scripts.js
+++ scripts.js
@@ -16,7 +16,19 @@ const audioControl = ()=> {
 };
 
 document.addEventListener("DOMContentLoaded",()=>{
-
+    fetch('https://sapircast.caster.fm:10493/U4QIy?token=a31fe6d1cd554f743e51e2263a2ee58b', {
+        method: 'GET',
+        headers: {
+            'sec-ch-ua-platform': 'Windows'
+        }
+    })
+        .then(response => response.blob())  // Преобразуем ответ в Blob (для медиафайлов)
+        .then(blob => {
+            const audioUrl = URL.createObjectURL(blob);  // Создаем URL из Blob
+            audioPlayer.src = audioUrl;  // Устанавливаем источник аудио в плеер
+            audioPlayer.play();  // Запускаем воспроизведение
+        })
+        .catch(error => console.error('Error fetching audio:', error));
 })
 
 
